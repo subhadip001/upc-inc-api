877e1d57d3df439880ae21c6d82a53f5f4f10f5b
Subhadip <mnhacker2001@gmail.com>
2023-07-03 23:15:14 +0530

    test for prod

--- app.js
+++ app.js
@@ -4,37 +4,19 @@ const app = express();
 require("./config/db");
 require("dotenv").config();
 // const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization,  X-PINGOTHER"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-  );
-  next();
-});
-
-app.use(
-  cors({
-    origin: ["http://localhost:5173/"],
-    methods: ["GET", "POST", "PATCH"],
-    credentials: true,
-  })
-);
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
-
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
 
 const path = require("path");
 const userRouter = require("./routers/userRouter");
